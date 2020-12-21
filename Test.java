477ae4837d614540aa6c9431b9c3ea4b31ec5f48
asteph <aliciap.stephens@gmail.com>
2020-12-21 01:53:58 +0000

    move

---
+++ Test.java
@@ -0,0 +1,42 @@
+public class Test {
+    public static void main(String[] args) {
+        my_method;
+    }
+
+    public static void my_method() {
+        System.out.println("Hello, world!");
+    }
+     
+    public class test customer {
+
+	// Constructors 
+	public Customer(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public Customer() {
+		super();
+	}
+	
+	// Getters & Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return lastName;
+	}
+	public void setSecondName(String secondName) {
+		this.lastName = secondName;
+	}   
+}
+}
